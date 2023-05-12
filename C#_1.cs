bda7305e72ddc130b1e3689b34a6b83c79b680aa
SunPixel <49581548+sunpixel@users.noreply.github.com>
2023-05-12 08:07:56 +0300

    C#_1.cs

--- C#_1.cs
+++ C#_1.cs
@@ -13,132 +13,112 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace C_
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        bool is_First;
-        int multiplication = 10;
-        int num = 0;
-        string number = string.Empty;
         public MainWindow()
         {
             InitializeComponent();
         }
-        // Clear everything
+
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
         private void Button_Clear_Click(object sender, RoutedEventArgs e)
         {
-            number.
+
         }
-        // Do a sign change
-        private void Button_change_sign_Click(object sender, RoutedEventArgs e)
+
+
+        private void Button_plus_minus_Click(object sender, RoutedEventArgs e)
         {
 
         }
-        // Precentegise number
-        private void Button_precent_Click(object sender, RoutedEventArgs e)
+
+        private void Button_Precentage_Click(object sender, RoutedEventArgs e)
         {
 
         }
-        // Do division operation on numbers
-        private void Button_divide_Click(object sender, RoutedEventArgs e)
+
+        private void Button_Division_Click(object sender, RoutedEventArgs e)
         {
 
         }
-        // Put 0
-        private void Button_zero_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                if (is_First)
-                {
-                    
-                }
-                else
-                {
 
-                }
-            }
-            catch
-            {
-
-            }
-        }
-        // Put 1
-        private void Button_one_Click(object sender, RoutedEventArgs e)
+        private void Button_7_Click(object sender, RoutedEventArgs e)
         {
 
         }
-        // put 2
-        private void Button_two_Click(object sender, RoutedEventArgs e)
+
+        private void Button_8_Click(object sender, RoutedEventArgs e)
         {
 
         }
-        // Put 3
-        private void Button_three_Click(object sender, RoutedEventArgs e)
+
+        private void Button_9_Click(object sender, RoutedEventArgs e)
         {
 
         }
-        // Put 4
-        private void Button_four_Click(object sender, RoutedEventArgs e)
+
+        private void Button_Multiplication_Click(object sender, RoutedEventArgs e)
         {
 
         }
-        // Put 5
-        private void Button_five_Click(object sender, RoutedEventArgs e)
+
+        private void Button_4_Click(object sender, RoutedEventArgs e)
         {
 
         }
-        // Put 6
-        private void Button_six_Click(object sender, RoutedEventArgs e)
+
+        private void Button_5_Click(object sender, RoutedEventArgs e)
         {
 
         }
-        // Put 7
-        private void Button_seven_Click(object sender, RoutedEventArgs e)
+
+        private void Button_6_Click(object sender, RoutedEventArgs e)
         {
 
         }
-        // Put 8
-        private void Button_eight_Click(object sender, RoutedEventArgs e)
-        {
 
-        }
-        // Put 9
-        private void Button_nine_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-        // Multiply numbers
-        private void Button_multiply_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-        // Do a minus operation on numbers
         private void Button_minus_Click(object sender, RoutedEventArgs e)
         {
 
         }
-        // Summurize the numbers
+
+        private void Button_1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
         private void Button_plus_Click(object sender, RoutedEventArgs e)
         {
 
         }
-        // Put a decimal point
-        private void Button_dot_Click(object sender, RoutedEventArgs e)
-        {
 
-        }
-        // Show results in the texbox window
-        private void Button_equals_Click(object sender, RoutedEventArgs e)
+        private void Button_0_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        private void Button_point_Click(object sender, RoutedEventArgs e)
         {
 
         }
