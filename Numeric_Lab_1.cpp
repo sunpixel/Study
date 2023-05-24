589a462ed10cb9a6b8f4a9b023fbe941c0cdd286
SunPixel <49581548+sunpixel@users.noreply.github.com>
2023-05-24 12:54:56 +0300

    Update Numeric_Lab_1.cpp

--- Numeric_Lab_1.cpp
+++ Numeric_Lab_1.cpp
@@ -1,130 +1,158 @@
-// number 12
+#include <vector>
+#include <iostream>
+#include <string>
+#include <locale.h>
+#include <cmath>
 
-#include<iostream>
-#include<math.h>
+using namespace std;
 
-double function(float a)
+double f(double x)   //x * e^ x = 1;
 {
-	a = 2 * sin(0.6 - a) + 1.5;
-	return a * -1;
+    return x * exp(x) - 1;
 }
 
-
-float method_1()
+double f1(double x)   // Первая производная (x + 1) * e ^ x = 0;
 {
-	
-	double a;
-	double b;
-	double eps;
-	bool correct = false;
-	while (not correct) 
-	{
-		std::cout << "Введите начало отрезка: ";
-		std::cin >> a;
-		std::cout << "Введите конеч отрезка: ";
-		std::cin >> b;
-		if (a <= b)
-		{
-			correct = true;
-		}
-		std::cout << "Введите желаемую точность: ";
-		std::cin >> eps;
-		if (abs(a) + abs(b) > eps)
-		{
-			correct = true;
-		}
-	}
+    return (x + 1) * exp(x);
+}
 
-	// fabs(12) абсолютное значение функции (модуль)
-
-	double c = 0;
-	int counter = 0;
-	while (fabs(b - a) > eps)
-	{
-		c = (a + b) / 2;
-		if (function(a) * function(c) < 0)
-		{
-			b = c;
-		}
-		else if (function(b) * function(c) < 0)
-		{
-			a = c;
-		}
-		else
-		{
-			std::cout << "No root found.\n";
-			break;
-		}
-		counter++;
-	}
-	std::cout << "x ~= " << c << "\nС точностью " << eps << "\nИ колличеством итераций = " << counter << std::endl;
-	return 0;
+double fi(double x)   //  x = 1 / e^x
+{
+    return 1 / exp(x);
 }
 
 
 
-float method_2()
+void method_1()
 {
-	return 0;
+    bool bul = true;
+    int i = 0;
+    double c, res;
+    double a = 0; double b = 0; double p = 0;
+
+    while (b <= a)
+    {
+        cout << "Введите начало отрезка: ";
+        cin >> a;
+        cout << "Введите конец отрезка: ";
+        cin >> b;
+        cout << "Введите точность: ";
+        cin >> p;
+        if (b <= a) cout << "Вы неправильно ввели отрезок" << '\n';
+        if (abs(b) + abs(a) < p) cout << "Точность превышает отрезок" << '\n';
+        if (f(a) * f(b) >= 0)
+        {
+            cout << "Единственного корня нет на этом отрезке" << '\n';
+            a = 0;
+            b = 0;
+        }
+    }
+    
+    c = (a + b) / 2;
+    while (true)
+    {
+        if (f(a) * f(c) < 0)
+            b = c;
+        else if (f(b) * f(c) < 0)
+            a = c;
+        i++;
+        c = (a + b) / 2;
+        if (f(c - p) * f(c + p) < 0) break;
+     
+    }    
+    cout << "x примерно равен " << c << " с точностью " << p << ". \nКоличество итераций: " << i << '\n'; 
 }
 
 
 
-bool next()
+void method_2()
 {
-	char answer;
-	bool loop = true;
-	while (loop)
-	{
-		std::cout << "Желаете продолжить? (y/n)\n";
-		std::cin >> answer;
-		if (answer == 'n' or answer == 'N')
-		{
-			return false;
-		}
-		else if (answer == 'y' or answer == 'Y')
-		{
-			return true;
-		}
-		else
-		{
-			std::cout << "Введенно не верное значение, введите 'y' or 'n' для продолжения.\n";
-		}
-	}
+    double c;
+    int i = 0;
+    double a = 0; double b = 0; double p = 0;
+
+    while (b <= a)
+    {
+        cout << "Введите начало отрезка: ";
+        cin >> a;
+        cout << "Введите конец отрезка: ";
+        cin >> b;
+        cout << "Введите точность: ";
+        cin >> p;        
+    }
+
+    c = a - (b - a) / (f(b) - f(a)) * f(a);
+    while (true)
+    {
+        if (f(a) * f(c) < 0)
+            b = c;
+        else if (f(b) * f(c) < 0)
+            a = c;
+
+        i++;        
+        if (f(c - p) * f(c + p) < 0) break;
+        c = a - (b - a) / (f(b) - f(a)) * f(a);
+    }
+    cout << "x примерно равен " << c << " с точностью " << p << ". \nКоличество итераций: " << i << '\n';
 }
 
 
 
-void choice()
+void method_3()
 {
-	int x;
-	bool loop = true;
-	while (loop)
-	{
-		std::cout << "Введите номер задания от 1-5 или 0 для завершения программы: ";
-		std::cin >> x;
-		if (x == 1)
-		{
-			method_1();
-			loop = next();
-		}
-		else if (x == 2)
-		{
-			method_2();
-			loop = next();
-		}
-		else
-		{
-			std::cout << "Выбранного задания не существует. Выберите другое!\n";
-		}
-	}
-	std::cout << "Завершение программы.\n";
+    long double x0 = 0;
+    long double x;
+    double p;
+    int i;
+
+    cout << "Введите начальное приближение корня: ";
+    cin >> x0;
+    cout << "Введите точность: ";
+    cin >> p;
+
+    i = 0;
+
+    while (true)
+    {
+        x = x0 - f(x0) / f1(x0);
+        i++;
+        if (f(x - p) * f(x + p) < 0) break;
+        x0 = x; 
+    }
+    cout << "x примерно равен " << x << " с точностью " << p << ". \nКоличество итераций: " << i << '\n';
+}
+
+
+void method_4()
+{
+    long double x0 = 0;
+    long double x, p;
+    int i = 0;
+    
+    cout << "Введите начальное приближение корня: ";
+    cin >> x0;
+    cout << "Введите точность: ";
+    cin >> p;
+
+    i = 0;
+    while (true)
+    {        
+        x = fi(x0);        
+        i++;
+        if (f(x - p) * f(x + p) < 0) break;
+        x0 = x;
+    }
+    cout << "x примерно равен " << x << " с точностью " << p << ". \nКоличество итераций: " << i;
 }
 
 
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-	choice();
+    setlocale(LC_ALL, "Russian"); 
+    method_1();
+    method_2();
+    method_3();
+    method_4();
 }
+
